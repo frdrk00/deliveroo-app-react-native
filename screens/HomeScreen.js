5a80dbcbbe062ea642628468360f52e8754767d7
frdrk00 <masterkooo@gmail.com>
2023-07-17 19:20:37 +0200

    HomeScreen - Search

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,4 +1,4 @@
-import { Image, Text, View } from 'react-native'
+import { Image, Text, TextInput, View } from 'react-native'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import {
   UserIcon,
@@ -9,7 +9,7 @@ import {
 
 const HomeScreen = () => {
   return (
-    <SafeAreaView>
+    <SafeAreaView className="bg-white pt-5">
       {/* Header */}
       <View className="flex-row pb-3 items-center mx-4 space-x-2">
         <Image
@@ -28,6 +28,18 @@ const HomeScreen = () => {
         </View>
 
         <UserIcon size={35} color="#00CCBB" />
+      </View>
+
+      {/* Search */}
+      <View className="flex-row items-center space-x-2 pb-2 mx-4">
+        <View className="flex-row flex-1 space-x-2 bg-gray-200 p-3">
+          <MagnifyingGlassIcon color="gray" size={20} />
+          <TextInput
+            placeholder="Restaurants and cuisines"
+            keyboardType="default"
+          />
+        </View>
+        <AdjustmentsVerticalIcon color="#00CCBB" />
       </View>
     </SafeAreaView>
   )
