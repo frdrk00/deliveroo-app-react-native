8d2d5385f59cc6adb93715f0aefd89390dc6e77b
frdrk00 <masterkooo@gmail.com>
2023-07-17 19:16:25 +0200

    HomeScreen - Header

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,12 +1,34 @@
-import { Text } from 'react-native'
+import { Image, Text, View } from 'react-native'
 import { SafeAreaView } from 'react-native-safe-area-context'
+import {
+  UserIcon,
+  ChevronDownIcon,
+  MagnifyingGlassIcon,
+  AdjustmentsVerticalIcon,
+} from 'react-native-heroicons/outline'
 
 const HomeScreen = () => {
-
-
   return (
     <SafeAreaView>
-      <Text>HomeScreen</Text>
+      {/* Header */}
+      <View className="flex-row pb-3 items-center mx-4 space-x-2">
+        <Image
+          source={{
+            uri: 'https://links.papareact.com/wru',
+          }}
+          className="h-7 w-7 bg-gray-300 p-4 rounded-full"
+        />
+
+        <View className="flex-1">
+          <Text className="font-bold text-gray-400 text-xs">Deliver Now!</Text>
+          <Text className="font-bold text-xl">
+            Current Location
+            <ChevronDownIcon size={20} color="#00CCBB" />
+          </Text>
+        </View>
+
+        <UserIcon size={35} color="#00CCBB" />
+      </View>
     </SafeAreaView>
   )
 }
