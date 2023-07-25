281caf7f0fc65a397e6d29d8ade7b261b357e027
frdrk00 <masterkooo@gmail.com>
2023-07-25 19:28:01 +0200

    Preparing Order Screen

--- App.js
+++ App.js
@@ -2,31 +2,37 @@ import { NavigationContainer } from '@react-navigation/native'
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
 import { Provider } from 'react-redux'
 import { store } from './store'
-import { BasketScreen, HomeScreen, RestaurantScreen } from './screens'
+import {
+  BasketScreen,
+  HomeScreen,
+  PreparingOrderScreen,
+  RestaurantScreen,
+} from './screens'
 
 import 'react-native-url-polyfill/auto'
 
 const Stack = createNativeStackNavigator()
-
 
 export default function App() {
   return (
     <NavigationContainer>
       <Provider store={store}>
         <Stack.Navigator screenOptions={{ headerShown: false }}>
+          <Stack.Screen name="Home" component={HomeScreen} />
+          <Stack.Screen name="Restaurant" component={RestaurantScreen} />
 
-            <Stack.Screen name="Home" component={HomeScreen} />
-            <Stack.Screen name="Restaurant" component={RestaurantScreen} />
-
-            <Stack.Screen
-            
-              name="Basket"
-              component={BasketScreen}
-              options={{
-                presentation: 'modal',
-              }}
-            />
-
+          <Stack.Screen
+            name="Basket"
+            component={BasketScreen}
+            options={{
+              presentation: 'modal',
+            }}
+          />
+          <Stack.Screen
+            name="PreparingOrder"
+            component={PreparingOrderScreen}
+            options={{ presentation: 'fullScreenModal' }}
+          />
         </Stack.Navigator>
       </Provider>
     </NavigationContainer>
--- screens/index.js
+++ screens/index.js
@@ -1,3 +1,4 @@
 export { default as HomeScreen } from './HomeScreen'
 export { default as RestaurantScreen } from './RestaurantScreen'
 export { default as BasketScreen } from './BasketScreen'
+export { default as PreparingOrderScreen } from './PreparingOrderScreen'
